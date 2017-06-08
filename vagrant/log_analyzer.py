b0aa3968b94b66e736d6511abc5cd4dcdd17a166
byanofsky <byanofsky@me.com>
2017-06-08 11:00:33 -0700

    Build init db to initialize db views

--- vagrant/log_analyzer.py
+++ vagrant/log_analyzer.py
@@ -12,15 +12,44 @@ def db_op(sql=None, data=None):
     with conn:
         with conn.cursor() as curs:
             curs.execute(sql, data)
-            output = curs.fetchall()
+            try:
+                output = curs.fetchall()
+            except psycopg2.ProgrammingError as e:
+                print(e)
+                output = None
     conn.close()
     return output
 
 
+def init_db():
+    view_total_visits = '''
+        CREATE VIEW day_visits_total AS
+             SELECT date_trunc('day', log.time) AS day,
+                    COUNT(log.id)
+               FROM log
+              GROUP BY day;
+    '''
+    db_op(view_total_visits)
+
+    view_error_visits = '''
+        CREATE VIEW day_visits_errors AS
+             SELECT date_trunc('day', log.time) AS day,
+                    COUNT(log.id)
+               FROM log
+              WHERE log.status != '200 OK'
+              GROUP BY day;
+    '''
+    db_op(view_error_visits)
+
+
 def get_three_popular_articles():
     sql = '''
-        SELECT title, visits
-          FROM article_visits
+        SELECT articles.title,
+               count(log.id) as visits
+          FROM articles,
+               log
+         WHERE log.path = '/article/' || articles.slug
+         GROUP BY articles.title
          ORDER BY visits DESC
          LIMIT 3;
     '''
@@ -30,11 +59,14 @@ def get_three_popular_articles():
 
 def get_popular_authors():
     sql = '''
-        SELECT authors.name AS author_name,
-               SUM(article_visits.visits) AS visits
-          FROM authors, article_visits
-         WHERE authors.id = article_visits.author_id
-         GROUP BY authors.id
+        SELECT authors.name,
+               count(log.id) as visits
+          FROM articles,
+               authors,
+               log
+         WHERE log.path = '/article/' || articles.slug
+               AND authors.id = articles.author
+         GROUP BY authors.name
          ORDER BY visits DESC;
     '''
     data = db_op(sql)
@@ -79,7 +111,7 @@ if __name__ == '__main__':
     formatted_articles_data = format_data(articles_data)
     popular_articles_table = create_table(formatted_articles_data,
                                           'Popular Articles')
-    # print(popular_articles_table)
+    print(popular_articles_table)
 
     author_data = get_popular_authors()
     formatted_author_data = format_data(author_data)
@@ -89,99 +121,4 @@ if __name__ == '__main__':
     error_data = get_error_data()
     formatted_error_data = format_data(error_data)
     error_table = create_table(formatted_error_data, 'Error Report')
-    # print(error_table)
-
-
-# # Get count of path visits for 200 status only
-# SELECT log.path, COUNT(log.id)
-#   FROM log
-#  WHERE log.status = '200 OK'
-#  GROUP BY log.path;
-#
-# # slug visit count (200 only)
-# CREATE VIEW slug_visits AS
-#        SELECT log.path AS slug,
-#               COUNT(log.id) AS visits
-#          FROM log
-#         WHERE log.status = '200 OK'
-#         GROUP BY log.path;
-#
-# # most popular articles
-# SELECT articles.title AS title,
-#        slug_visits.slug AS slug,
-#        slug_visits.visits AS visits
-#   FROM articles, slug_visits
-#  WHERE slug_visits.slug = '/article/' || articles.slug
-#  ORDER BY visits DESC;
-#
-# # article visits
-# CREATE VIEW article_visits AS
-#        SELECT articles.id AS article_id,
-#               articles.title AS title,
-#               articles.author AS author_id,
-#               slug_visits.slug AS slug,
-#               slug_visits.visits AS visits
-#          FROM articles, slug_visits
-#         WHERE slug_visits.slug = '/article/' || articles.slug
-#         ORDER BY visits DESC;
-#
-# # 3 most popular articles
-# SELECT title, visits
-#   FROM article_visits
-#  ORDER BY visits DESC
-#  LIMIT 3;
-#
-# # Popular authors with count
-# SELECT authors.name AS author_name,
-#        authors.id AS author_id,
-#        SUM(article_visits.visits) AS visits
-# FROM authors, article_visits
-# WHERE authors.id = article_visits.author_id
-# GROUP BY authors.id
-# ORDER BY visits DESC;
-#
-# # Convert date to char
-# to_char(date_trunc('day', log.time), 'Month DD, YYYY')
-#
-# # Total visits by day
-# CREATE VIEW day_visits_total AS
-#      SELECT date_trunc('day', log.time) AS day,
-#             COUNT(log.id)
-#        FROM log
-#       GROUP BY day;
-#
-# # Success visits by day
-# CREATE VIEW day_visits_success AS
-#      SELECT date_trunc('day', log.time) AS day,
-#             COUNT(log.id)
-#        FROM log
-#       WHERE log.status = '200 OK'
-#       GROUP BY day;
-#
-# # Error visits each day
-# CREATE VIEW day_visits_errors AS
-#      SELECT date_trunc('day', log.time) AS day,
-#             COUNT(log.id)
-#        FROM log
-#       WHERE log.status != '200 OK'
-#       GROUP BY day;
-#
-# # Show all log visits
-# SELECT day_visits_total.day as day,
-#        day_visits_total.count as total,
-#        day_visits_errors.count as errors,
-#        day_visits_success.count as success,
-#        day_visits_errors.count + day_visits_success.count = day_visits_total.count as check
-#   FROM day_visits_total,
-#        day_visits_errors,
-#        day_visits_success
-#  WHERE day_visits_total.day = day_visits_errors.day
-#        AND day_visits_total.day = day_visits_success.day;
-#
-# # Show percentage of errors
-# SELECT day_visits_total.day as day,
-#        CAST(day_visits_errors.count AS real) / day_visits_total.count as error_perc
-#   FROM day_visits_total,
-#        day_visits_errors
-#  WHERE day_visits_total.day = day_visits_errors.day
-#        AND CAST(day_visits_errors.count AS real) / day_visits_total.count > 0.01;
+    print(error_table)
